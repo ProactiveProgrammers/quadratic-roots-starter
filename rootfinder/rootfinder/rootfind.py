26321859e37b71c6e94d86829a19fb9327517145
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-09-14 22:28:06 -0400

    feat: Add the template version of rootfind with TODO markers.

---
+++ rootfinder/rootfinder/rootfind.py
@@ -0,0 +1,20 @@
+"""Calculate the roots of a quadratic equation."""
+
+from typing import Tuple
+from typing import Union
+
+
+def calculate_quadratic_equation_roots(
+    a: float, b: float, c: float
+) -> Tuple[Union[float, complex], Union[float, complex]]:
+    """Calculate the roots of a quadratic equation."""
+    # TODO: Use the source code example on page 21 of the
+    # "Doing Math with Python to implement this function.
+    # TODO: Your function should take as input the values
+    # of a, b, and c and return the values of x_one
+    # and x_two as a two-tuple of values.
+    # TODO: The return values of this function can either
+    # be a complex number (i.e., involving an imaginary
+    # number and designated with j) or a floating-point
+    # value (i.e., not involving an imaginary number and
+    # instead only a decimal value)
